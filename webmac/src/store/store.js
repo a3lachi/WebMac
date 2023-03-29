b6294c9f0d5d1f07dfa4a73b131f41f60139aa33
a3lachi <mad.aalachi@gmail.com>
2023-03-29 05:17:13 +0200

    code update412

--- webmac/src/store/store.js
+++ webmac/src/store/store.js
@@ -88,6 +88,8 @@ export default {
         ){
           console.log('on border')
           state.resize.status = true
+          state.mouse.x = ev.clientX
+          state.mouse.y = ev.clientY
         }
         else {
           state.drag.view.pop()
@@ -129,7 +131,7 @@ export default {
           console.log('moving on border')
           const deltaX = ev.clientX - state.mouse.x;
           const deltaY = ev.clientY - state.mouse.y;
-          console.log(deltaX)
+          console.log(state.win[elem])
           // state.mouse.x = ev.clientX
           // state.mouse.y = ev.clientY
           // state.win[elem].width = Math.max(
