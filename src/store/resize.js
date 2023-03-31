b03afdc7050852142c2ee8b44fcf8d7c0179bd88
a3laxi <mad.aalachi@gmail.com>
2023-03-31 06:49:03 +0200

    code update434

--- src/store/resize.js
+++ src/store/resize.js
@@ -1,14 +1,13 @@
 
 const borderSize = 6 ;
 
-export const resizeTop = (elem,delta) => {
-  elem.style.bottom = elem.style.top + elem.style.height
-  elem.style.removeProperty("top");
-  elem.style.height += delta
-}
-
 export const bordersCondition = (x,y,width,height) => {
   const bol =  x-1 < borderSize ? "lef" :  y-1 < borderSize ? "top" :  x-1 > width - borderSize ? "rig" :  y-1 > height - borderSize ? "bot" : false 
   return {bol:bol}
 }
 
+export const resizeTop = (elem,delta) => {
+  elem.style.bottom = elem.style.top + elem.style.height
+  elem.style.removeProperty("top");
+  elem.style.height += delta
+}
