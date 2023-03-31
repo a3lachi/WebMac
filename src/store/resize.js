566c781d46d8a44ddd01033c227e502cdf9bff89
a3laxi <mad.aalachi@gmail.com>
2023-03-31 06:50:37 +0200

    code update437

--- src/store/resize.js
+++ src/store/resize.js
@@ -7,7 +7,7 @@ export const bordersCondition = (x,y,width,height) => {
 }
 
 export const resizeTop = (elem,delta) => {
-  console.log(elem.style.top,elem.style.height)
+  console.log(elem)
   // elem.style.bottom = elem.style.top + elem.style.height
   // elem.style.removeProperty("top");
   // elem.style.height += delta
