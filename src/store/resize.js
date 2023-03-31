a3802d1fea4104ffbe08e1195fc4efd01416b01a
a3laxi <mad.aalachi@gmail.com>
2023-03-31 06:59:23 +0200

    code update454

--- src/store/resize.js
+++ src/store/resize.js
@@ -5,12 +5,15 @@ export const bordersCondition = (elem,x,y,width,height) => {
   const bol =  x-1 < borderSize ? "lef" :  y-1 < borderSize ? "top" :  x-1 > width - borderSize ? "rig" :  y-1 > height - borderSize ? "bot" : false 
   
   if (bol==='top') {
+    elem.style.bottom = elem.style.top.split('px')[0] + height + 'px'
     elem.style.removeProperty("top");
+    elem.style.position = 'fixed'
+
   }
   return {bol:bol}
 }
 
 export const resizeTop = (elem,delta,height) => {
-  elem.style.height = delta + height + 'px'
+  // elem.style.height = delta + height + 'px'
 
 }
